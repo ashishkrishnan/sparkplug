5fd9baece3dc16df4f0bdefbcb86805815b91b2b
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-21 23:38:56 +0530

    Fix issues with Multiple Definition" in the config file.
    Defining variables (allocating memory for them) inside your header files (config.h and local.h). Because these headers are included by multiple .cpp files (sparkplug.ino, connectivity.cpp, and api.cpp), the compiler creates a separate copy of WIFI_SSID, HOSTNAME, etc., for each file. When the linker tries to combine them into the final firmware, it sees duplicates and crashes.

--- src/config/config.h
+++ src/config/config.h
@@ -3,23 +3,24 @@
 #include <Arduino.h>
 #include "local.h"
 
-const char* HOSTNAME = "sparkplug";
+static const char* HOSTNAME = "sparkplug";
 
 // Wifi Configuration
-const char* WIFI_SSID = LOCAL_WIFI_SSID;
-const char* WIFI_PASS = LOCAL_WIFI_PASSWORD;
+static const char* WIFI_SSID = LOCAL_WIFI_SSID;
+static const char* WIFI_PASS = LOCAL_WIFI_PASSWORD;
 
-constexpr int BOOT_DELAY_IN_MS = LOCAL_BOOT_DELAY_IN_MS;
+static constexpr int BOOT_DELAY_IN_MS = LOCAL_BOOT_DELAY_IN_MS;
 
-const char* OS_NAME_PRIMARY = LOCAL_OS_NAME_PRIMARY;
-const char* OS_NAME_SECONDARY = LOCAL_OS_NAME_SECONDARY;
+static const char* OS_NAME_PRIMARY = LOCAL_OS_NAME_PRIMARY;
+static const char* OS_NAME_SECONDARY = LOCAL_OS_NAME_SECONDARY;
 
-constexpr int GRUB_SECONDARY_OS_POSITION = LOCAL_GRUB_SECONDARY_OS_POSITION;
+static constexpr int GRUB_SECONDARY_OS_POSITION = LOCAL_GRUB_SECONDARY_OS_POSITION;
 
-const float MAX_TEMP_C = 85.0;
+static const float MAX_TEMP_C = 85.0;
 
-const char* TARGET_PC_IP_ADDRESS = LOCAL_TARGET_PC_IP_ADDRESS;
+static const char* TARGET_PC_IP_ADDRESS = LOCAL_TARGET_PC_IP_ADDRESS;
 
-constexpr uint8_t PIN_RELAY = 4;
+static constexpr uint8_t PIN_RELAY = 4;
+
 
 #endif
