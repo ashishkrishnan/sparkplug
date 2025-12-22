5df80960f6e5400ce4f18dae8d9fc467bb9c93fe
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-22 21:27:00 +0530

    Add logs to the system health api from Relay sequences.

--- sparkplug.ino
+++ sparkplug.ino
@@ -12,25 +12,44 @@
   #include "src/tests/TestRunner.h"
 #endif
 
-Relay hwRelay; 
-USBKeyboard hwKb; 
+Relay hwRelay;
+USBKeyboard hwKb;
 Health hwHealth;
+
 Connectivity network;
 
-Power power(&hwRelay); 
-Boot boot(&hwKb); 
+Power power(&hwRelay);
+Boot boot(&hwKb);
 Safety safety(&hwHealth);
 WebService web_service;
 
 #ifndef RUN_TESTS_ON_BOOT
+
 void executeWake(String os) {
-    if(!safety.isSafeToOperate()) return;
+    if(!safety.isSafeToOperate()) {
+        web_service.logEvent("SEQ: Aborted. Thermal/Safety Lock.");
+        return;
+    }
+
+    web_service.logEvent("[Wake] Pulsing Relay (Wake)...");
     power.triggerPulse();
+
+    web_service.logEvent("[Wake] Waiting " + String(BOOT_DELAY_IN_MS/1000) + "s for BIOS...");
+
     boot.selectOS(os);
+
+    web_service.logEvent("[Wake] Success. Selected " + os);
 }
 
 void executeShutdown() {
-    if(safety.isSafeShutdownAllowed()) power.triggerPulse();
+    web_service.logEvent("[Shutdown] Shutdown Requested.");
+
+    if(safety.isSafeShutdownAllowed()) {
+        web_service.logEvent("[Shutdown] Target is ON. Pulsing Relay...");
+        power.triggerPulse();
+    } else {
+        web_service.logEvent("[Shutdown] Target already OFF. Ignored.");
+    }
 }
 #endif
 
@@ -40,16 +59,20 @@ void setup() {
 
 #ifdef RUN_TESTS_ON_BOOT
     Serial.println("--- STARTING TEST SUITE ---");
-    runSuite(); // Run tests once
+    runSuite();
     Serial.println("--- TESTS COMPLETE ---");
-    while(1) delay(1000); // Halt system
+    while(1) delay(1000);
 #else
     // Production Setup
+    web_service.logEvent("[Sparkplug] Starting system");
+
     power.setup();
     network.setupConnectivity();
 
     // Pass the callbacks
     web_service.setupWebAPI(executeWake, executeShutdown);
+
+    web_service.logEvent("[Sparkplug] Boot Complete. Ready.");
 #endif
 }
 
