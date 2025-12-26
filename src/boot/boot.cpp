6e98aa4e05dbf4258997eb35a07c73c7a85f11ed
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-26 10:31:12 +0530

    Remove deprecated methods.

--- src/boot/boot.cpp
+++ src/boot/boot.cpp
@@ -6,7 +6,6 @@
 #include "usbkeyboard.h"
 
 Boot::Boot(IKeyboard *kb, Logger logger) : _kb(kb), _logger(std::move(logger)) {
-    _isBusy = false;
     _lastSequenceFinishTime = 0;
     _state = IDLE;
 }
@@ -23,35 +22,6 @@ long Boot::getCoolDownRemaining() {
     if (!isCoolingDown()) return 0;
     unsigned long elapsed = (millis() - _lastSequenceFinishTime) / 1000;
     return POST_BOOT_COOL_DOWN_IN_SECONDS - elapsed;
-}
-
-String Boot::getStateName() {
-    switch(_state) {
-        case IDLE: return "IDLE";
-        case WAITING_FOR_BOOT: return "WAITING_BIOS";
-        case NAVIGATING: return "NAVIGATING";
-        case SHUTTING_DOWN: return "SHUTTING_DOWN";
-        case COOLING_DOWN: return "COOLING_DOWN";
-        default: return "UNKNOWN";
-    }
-}
-
-void Boot::selectOS(const String &os, const String &strategy) {
-    if (_isBusy) return;
-    _isBusy = true;
-
-    _logger("[BOOT] Sequence Started for " + os);
-
-    if (strategy == "aggressive") {
-        performAggressiveWait();
-    } else {
-        performStandardWait();
-    }
-
-    performNavigation(os);
-
-    _isBusy = false;
-    _lastSequenceFinishTime = millis();
 }
 
 void Boot::startSequence(String os, String strategy) {
@@ -138,51 +108,6 @@ void Boot::performNavigation() {
         _kb->pressKey(KEY_ENTER);
         delay(100); _kb->releaseAll();
     }
-}
-
-void Boot::performStandardWait() const {
-    _logger("[BOOT] Standard Wait (" + String(TIME_TAKEN_TO_REACH_BOOT_MENU_IN_MILLIS) + "ms)");
-    delay(TIME_TAKEN_TO_REACH_BOOT_MENU_IN_MILLIS);
-}
-
-void Boot::performAggressiveWait() const {
-    _logger("[BOOT] Aggressive Wait (Heartbeat Active)");
-    long startTime = millis();
-    long lastHeartbeat = 0;
-
-    while (millis() - startTime < TIME_TAKEN_TO_REACH_BOOT_MENU_IN_MILLIS) {
-        if (millis() - lastHeartbeat > 2000) {
-            _kb->pressKey(KEY_SHIFT);
-            delay(50);
-            _kb->releaseAll();
-            lastHeartbeat = millis();
-        }
-        delay(100);
-    }
-}
-
-void Boot::performNavigation(String os) const {
-    _logger("[BOOT] Navigating Menu...");
-
-    if (os == OS_NAME_SECONDARY) {
-        // Windows
-        for (int i = 0; i < GRUB_SECONDARY_OS_POSITION - 1; i++) {
-            _kb->pressKey(KEY_DOWN);
-            delay(150);
-            _kb->releaseAll();
-            delay(350);
-        }
-        delay(500);
-        _kb->pressKey(KEY_ENTER);
-        delay(150);
-        _kb->releaseAll();
-    } else {
-        // Ubuntu
-        _kb->pressKey(KEY_ENTER);
-        delay(100);
-        _kb->releaseAll();
-    }
-    _logger("[BOOT] Done");
 }
 
 #endif //BOOT_CPP_H
