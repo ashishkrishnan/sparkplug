af7cf2d4d22bf254a5dd9bc9ad22398f31ab8523
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-27 23:20:48 +0530

    Transform WoL into a router for selecting OSes based on virtual mac address.

--- src/wakeonlan/wol.cpp
+++ src/wakeonlan/wol.cpp
@@ -4,9 +4,11 @@
 #include "wol.h"
 
 #include <utility>
-#include "../safety/safety.h"
 #include "../system/systeminfo.h"
 #include "../logger/EventLogger.h"
+#include "../connectivity/connectivity.h"
+
+extern Connectivity network;
 
 Wol::Wol() {}
 
@@ -27,16 +29,40 @@ void Wol::setupWol(WakeCallback onWakeUpCallback) {
 }
 
 void Wol::handleWolLoop() {
-    int size = Udp.parsePacket();
-    if (size == 102) {
-        Udp.read(packetBuffer, 102);
-        if (packetBuffer[0] == 0xFF) {
-            Log.log("[WoL] Wake Packet Received");
-            if (!isThermalUnsafe() && onWakeUp) {
-                // Wake Ubuntu (Default) with Default Strategy
-                onWakeUp(OS_NAME_PRIMARY, DEFAULT_BOOT_STRATEGY);
-            }
-        }
+    int packetSize = Udp.parsePacket();
+    if (packetSize != 102) return;
+
+    Udp.read(packetBuffer, 102);
+
+    for (int i = 0; i < 6; i++) {
+        if (packetBuffer[i] != 0xFF) return;
+    }
+
+    uint8_t receivedMacAddress[6];
+    memcpy(receivedMacAddress, &packetBuffer[6], 6);
+
+    uint8_t actualMacAddress[6];
+    network.getMacBytes(actualMacAddress);
+
+    uint8_t primaryMac[6];
+    memcpy(primaryMac, actualMacAddress, 6);
+    primaryMac[5] = VIRTUAL_MAC_HEX_PRIMARY;
+
+    uint8_t secondaryMac[6];
+    memcpy(secondaryMac, actualMacAddress, 6);
+    secondaryMac[5] = VIRTUAL_MAC_HEX_SECONDARY;
+
+    if (isThermalUnsafe()) return;
+
+    if (memcmp(actualMacAddress, primaryMac, 6) == 0) {
+        Log.log("[WoL] Waking up: " + String(OS_NAME_PRIMARY));
+        if (onWakeUp) onWakeUp(OS_NAME_PRIMARY, DEFAULT_BOOT_STRATEGY);
+    } else if (memcmp(actualMacAddress, secondaryMac, 6) == 0) {
+        Log.log("[WoL] Waking up: " + String(OS_NAME_SECONDARY));
+        if (onWakeUp) onWakeUp(OS_NAME_SECONDARY, DEFAULT_BOOT_STRATEGY);
+    } else if (memcmp(receivedMacAddress, actualMacAddress, 6) == 0) {
+        Log.log("[WoL] Waking up default OS: " + String(OS_NAME_PRIMARY));
+        if (onWakeUp) onWakeUp(OS_NAME_PRIMARY, DEFAULT_BOOT_STRATEGY);
     }
 }
 
