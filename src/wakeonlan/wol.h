4c5b32ecf5b5248e2cc296727d9591a73c408f4a
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-27 09:18:02 +0530

    Add a wol.h file with contracts for WakeOnLan.

---
+++ src/wakeonlan/wol.h
@@ -0,0 +1,25 @@
+#ifndef WOL_H
+#define WOL_H
+#include <Arduino.h>
+#include <WiFiUdp.h>
+
+#include "logger/EventLogger.h"
+
+typedef void (*WakeCallback)(String os, String strategy);
+
+class Wol {
+private:
+    WiFiUDP Udp;
+    WakeCallback onWakeUp;
+    // Helper
+    byte packetBuffer[102];
+
+    EventLogger logger;
+public:
+    Wol();
+
+    void setupWol(WakeCallback onWakeUpCallback);
+
+    void handleWolLoop();
+};
+#endif //WOL_H
