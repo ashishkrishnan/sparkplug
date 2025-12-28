8f5b62b7a46dd70fe63be6ebd006b56ab2e075ec
Ashish Krishnan <ashishkrish09@gmail.com>
2025-12-28 14:06:43 +0530

    Add fixes for WoL callback setup which failed for retrigging.
    For now, keep the callback on the sparkplug entry point.

--- src/wakeonlan/wol.cpp
+++ src/wakeonlan/wol.cpp
@@ -10,16 +10,20 @@
 
 extern Connectivity network;
 
-Wol::Wol(){}
+Wol::Wol() {
+    memset(_systemMac, 0, 6);
+}
 
 void Wol::setupWol(WakeCallback onWakeUpCallback) {
     onWakeUp = onWakeUpCallback;
     Udp.begin(WOL_PORT);
+    network.getMacBytes(_systemMac);
+    Log.log("[WoL] Setup complete");
 }
 
 void Wol::handleWolLoop() {
     int packetSize = Udp.parsePacket();
-    if (packetSize != 102) return;
+    if (packetSize < 102) return;
 
     Udp.read(packetBuffer, 102);
 
@@ -27,28 +31,25 @@ void Wol::handleWolLoop() {
         if (packetBuffer[i] != 0xFF) return;
     }
 
-    uint8_t receivedMacAddress[6];
-    memcpy(receivedMacAddress, &packetBuffer[6], 6);
+    uint8_t receivedMac[6];
+    memcpy(receivedMac, &packetBuffer[6], 6);
 
-    uint8_t actualMacAddress[6];
-    network.getMacBytes(actualMacAddress);
+    uint8_t actualMac[6];
+    memcpy(actualMac, _systemMac, 6);
 
     uint8_t primaryMac[6];
-    memcpy(primaryMac, actualMacAddress, 6);
+    memcpy(primaryMac, actualMac, 6);
     primaryMac[5] = VIRTUAL_MAC_HEX_PRIMARY;
 
     uint8_t secondaryMac[6];
-    memcpy(secondaryMac, actualMacAddress, 6);
+    memcpy(secondaryMac, actualMac, 6);
     secondaryMac[5] = VIRTUAL_MAC_HEX_SECONDARY;
 
-    if (memcmp(actualMacAddress, primaryMac, 6) == 0) {
-        Log.log("[WoL] Waking up: " + String(OS_NAME_PRIMARY));
+    if (memcmp(receivedMac, primaryMac, 6) == 0) {
         if (onWakeUp) onWakeUp(OS_NAME_PRIMARY, DEFAULT_BOOT_STRATEGY);
-    } else if (memcmp(actualMacAddress, secondaryMac, 6) == 0) {
-        Log.log("[WoL] Waking up: " + String(OS_NAME_SECONDARY));
+    } else if (memcmp(receivedMac, secondaryMac, 6) == 0) {
         if (onWakeUp) onWakeUp(OS_NAME_SECONDARY, DEFAULT_BOOT_STRATEGY);
-    } else if (memcmp(receivedMacAddress, actualMacAddress, 6) == 0) {
-        Log.log("[WoL] Waking up default OS: " + String(OS_NAME_PRIMARY));
+    } else if (memcmp(receivedMac, actualMac, 6) == 0) {
         if (onWakeUp) onWakeUp(OS_NAME_PRIMARY, DEFAULT_BOOT_STRATEGY);
     }
 }
